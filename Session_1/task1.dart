bf6895e4997cb030f08a2fdc40da172b30e9b710
jaysolanki6 <154721240+jaysolanki6@users.noreply.github.com>
2026-09-07 16:18:22 +0530

    Update task1.dart

--- Session_1/task1.dart
+++ Session_1/task1.dart
@@ -1,1 +1,12 @@
-
+void main()
+{
+   String  foodname = "pizza";
+   double price_of_food = 250.50;
+   bool isvegetarian = false;
+  
+  print(foodname);
+  print(price_of_food);
+  print(isvegetarian);
+  
+  
+}
