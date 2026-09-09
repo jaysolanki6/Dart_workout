989ccbea8eeaeb8f958b5104ab866ea35b2b1a81
jaysolanki6 <154721240+jaysolanki6@users.noreply.github.com>
2026-09-09 15:29:41 +0530

    Update task2.dart

--- Session_2/task2.dart
+++ Session_2/task2.dart
@@ -1,1 +1,13 @@
+bool checkDiscountEligibility(double amount) {
+  return amount >= 500;
+}
 
+void main() {
+  var amount = 650.50;
+
+  if (checkDiscountEligibility(amount)) {
+    print("You are eligible for a discount!");
+  } else {
+    print("No discount available.");
+  }
+}
