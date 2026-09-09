916d7edcf4229675c14690c91677572f1ad49d2e
jaysolanki6 <154721240+jaysolanki6@users.noreply.github.com>
2026-09-09 14:55:33 +0530

    Create task5.dart

---
+++ Session_1/task5.dart
@@ -0,0 +1,22 @@
+void main() {
+// String is used because a name contains text.
+String name = "Jay Solanki";
+
+// int is used because age is a whole number without decimals.
+int age = 20;
+
+// String is used because an email address contains text and symbols.
+String email = "[jay@example.com](mailto:jay@example.com)";
+
+// double is used because price can contain decimal values.
+double price = 69999.0;
+
+// bool is used because the product can be either in stock or not.
+bool isInStock = true;
+
+print("Name: $name");
+print("Age: $age");
+print("Email: $email");
+print("Price: $price");
+print("In Stock: $isInStock");
+}
