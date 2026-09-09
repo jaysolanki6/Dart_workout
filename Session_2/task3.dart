c53aedf43cb58d90433019d877c7deec60b45d77
jaysolanki6 <154721240+jaysolanki6@users.noreply.github.com>
2026-09-09 15:45:48 +0530

    Update task3.dart

--- Session_2/task3.dart
+++ Session_2/task3.dart
@@ -1,23 +1,22 @@
+import 'dart:math';
+
 void main()
 {
-  double product1 = 500.0;
-  double product2 = 400.0;
-  double product3 = 300.0;
+  double product1 = 500.00;
+  double product2 = 300.00;
+  double product3 = 200.00;
+  double product4 = 100.00;
 
-  double total = product1 + product2 + product3;
-
-  print("Total Amount: $total");
-
-  if (total > 1000) {
-    double discount = total * 0.10;
-    double finalAmount = total - discount;
-
-
-  print("10% Discount Applied!");
-  print("Final Amount: $finalAmount");
+  double total = product1+product2+product3+product4;
+  print("total amount $total");
+  if(total>=1000){
+    double discount = total*0.10;
+    double finleamount = total-discount;
+    print("10% Discount Applied!");
+    print("finle amount $finleamount");
   }
-  else {
-  print("No Discount Available.");
-  print("Final Amount: $total");
+    else{
+      print("No discount avelabe ");
+      print("finle amout $total ");
   }
 }
