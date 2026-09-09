46bc4ed32b7483025f9f6a37fe2a3ade3f4c52fd
jaysolanki6 <154721240+jaysolanki6@users.noreply.github.com>
2026-09-09 15:14:37 +0530

    Update task1.dart

--- Session_2/task1.dart
+++ Session_2/task1.dart
@@ -1,1 +1,12 @@
+void main()
+{
+  double baseprice = 200.20;
+  double gst = baseprice*0.12;
 
+  var finaleprice = baseprice+gst;
+
+  print(" base price: $baseprice");
+  print(" gst: $gst");
+  print(" finaleprice: $finaleprice");
+
+}
